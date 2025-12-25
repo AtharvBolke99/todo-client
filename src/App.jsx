58ad4e28ffbfd222ab1f5b185a511208fc01becc
AtharvBolke99 <atharvbolke99@gmail.com>
2025-12-26 00:22:01 +0530

    todo app created with the help of the server

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Toaster, toast } from "react-hot-toast";
+import { SquarePen, Trash } from "lucide-react";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const [oldTodo, setOldTodo] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+
+  const BASE_URL = "http://localhost:8080";
+
+  const loaddata = async () => {
+    const response = await axios.get(`${BASE_URL}`);
+    // console.log(response.data.data);
+    setTodos(response.data.data);
+  };
+
+  const addTask = async () => {
+    await axios.post(`${BASE_URL}/todos`, {
+      todo: newTask,
+    });
+    loaddata();
+    // console.log(response.dada.data);
+  };
+
+  const deleteTask = async (todo) => {
+    await axios.delete(`${BASE_URL}/todos`, {
+      data: { todo: todo },
+    });
+    loaddata();
+  };
+
+  const updateTask = async (oldTodo, newTask) => {
+    await axios.put(`${BASE_URL}/todos`, {
+      oldTodo: oldTodo,
+      newTodo: newTask,
+    });
+    loaddata();
+  };
+
+   useEffect(() => {
+    loaddata();
+  }, []);
+  return (
+    <div>
+      <h1 className="header">ToDO App</h1>
+      <div className="task-card">
+        {todos.map((todo, index) => {
+          return (
+            <div key={index} className="card">
+              {todo}
+              <div className="edit-delete-box">
+                <SquarePen
+                  onClick={() => {
+                    setIsEditing(true);
+                    setOldTodo(todo);
+                    setNewTask(todo);
+                  }}
+                />
+                <Trash
+                  onClick={() => {
+                    deleteTask(todo);
+                  }}
+                />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="input-box">
+        <input
+          className="todo-input-box"
+          type="text"
+          placeholder="Enter the Task"
+          value={newTask}
+          onChange={(e) => {
+            setNewTask(e.target.value);
+          }}
+        />
+        <button
+          className="btn"
+          onClick={() => {
+            if (newTask === "") {
+              toast.error("Enter The Task!");
+              return;
+            }
+
+            if (isEditing) {
+              updateTask(oldTodo, newTask);
+              setIsEditing(false);
+              setOldTodo("");
+              setNewTask("");
+              return;
+            } else {
+              addTask();
+              setNewTask("");
+            }
+          }}
+        >
+          {isEditing ? "Edit Task" : "Add Task"}
+        </button>
+      </div>
+      <Toaster />
+    </div>
+  );
+}
+
+export default App;
